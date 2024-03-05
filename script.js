280796ab020ee00945f9f2fa258608b884c230ba
MrAhsanKhann <159944912+MrAhsanKhann@users.noreply.github.com>
2024-03-05 06:13:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var length = 5
+var width = 4
+var area = length*width;
+
+console.log('length is = ' + length )
+console.log('width is = ' + width )
+console.log('Area of Rectangle = ' + area)
+
+var base = 5
+var height = 4
+var area = 1/2*base*height;
+
+console.log('base is = ' + base )
+console.log('height is = ' + height )
+console.log('Area of Triangle = ' + area)
+
+var Pi = 22/7
+var radius = 4
+var area = 22/7*(radius*radius);
+
+console.log('Pi is = ' + Pi )
+console.log('radius  is = ' + radius  )
+console.log('Area of Circle = ' + area)
