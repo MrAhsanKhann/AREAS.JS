788a8fc6d15004c2270559642237ba4cc17fc215
MrAhsanKhann <159944912+MrAhsanKhann@users.noreply.github.com>
2024-03-22 23:39:56 -0700

    Update script.js
    Three differnt methods used for calculating areas!!

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-var length = 5
-var width = 4
-var area = length*width;
+//Area of Rectangle
 
-console.log('length is = ' + length )
-console.log('width is = ' + width )
-console.log('Area of Rectangle = ' + area)
+function areaRectangle(length , width) {
+    return length*width;
+}
+console.log(areaRectangle(4,5));
 
+//Area of Triangle
 var base = 5
 var height = 4
 var area = 1/2*base*height;
 
-console.log('base is = ' + base )
-console.log('height is = ' + height )
-console.log('Area of Triangle = ' + area)
+// console.log('base is = ' + base )
+// console.log('height is = ' + height )
+// console.log('Area of Triangle = ' + area)
+messageTriangle = `
+Area of triangle
+base = 5
+height = 4
+area = ${1/2*base*height}
+`
+console.log(messageTriangle);
+
+// Calculating Area of R
 
 var Pi = 22/7
 var radius = 4
 var area = 22/7*(radius*radius);
 
-console.log('Pi is = ' + Pi )
-console.log('radius  is = ' + radius  )
-console.log('Area of Circle = ' + area)
+
+messageCircle = `
+Area of circle
+Pi = 22/7
+radius = 4
+area = ${area}
+`
+console.log(messageCircle);
